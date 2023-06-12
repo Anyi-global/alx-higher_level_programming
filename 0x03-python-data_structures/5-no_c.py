1df73e972100b8519718526511ae4a71cf24ce64
Anyi-global <ikeifeanyi95.ii@gmail.com>
2023-06-12 03:27:02 -0700

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    lst = list(my_string)
-    for x in lst:
-        if x == 'c':
-            lst.remove('c')
-        if x == 'C':
-            lst.remove('C')
-    new_string = "".join(lst)
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
     return new_string

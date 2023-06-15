c0d47400415c42f18a3db6debd399f4849e64675
Anyi-global <ikeifeanyi95.ii@gmail.com>
2023-06-15 03:35:32 -0700

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-	uniq_num = []
-	sum = 0
-	for num in my_list:
-		if num not in uniq_num:
-			uniq_num.append(num)
-	for num in uniq_num:
-		sum += num
-	return (sum)
+    uniq_num = []
+    sum = 0
+    for num in my_list:
+        if num not in uniq_num:
+            uniq_num.append(num)
+    for num in uniq_num:
+        sum += num
+    return (sum)
